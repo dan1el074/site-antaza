414c0798a480d9aa21eb3584febfc8ba56a6cf2d
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-09-30 00:27:23 -0300

    initial commit

---
+++ js/reveal.js
@@ -0,0 +1,10 @@
+
+window.revelar = ScrollReveal({reset: true})
+
+// HERO SECTION
+revelar.reveal('.left-animation', {
+    duration: 1500,
+    distance: '50px',
+    origin: 'right',
+    delay: 200
+})
